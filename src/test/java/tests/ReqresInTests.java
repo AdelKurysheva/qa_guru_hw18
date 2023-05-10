bf3438b6f0518c1f38536e5d2d2233ef5d8b502b
Adel Kurysheva <Adel.Kurysheva@nedra.digital>
2023-05-10 19:17:49 +0300

    add all

---
+++ src/test/java/tests/ReqresInTests.java
@@ -0,0 +1,87 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.CoreMatchers.is;
+import static io.restassured.RestAssured.given;
+
+public class ReqresInTests {
+    @Test
+    void createUser() {
+
+        String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+    @Test
+    void getNameSingleResource() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/unknown/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.name", is("fuchsia rose"))
+                .body("data.color", is("#C74375"));
+
+    }
+    @Test
+    void singleResourceNotFound() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/unknown/23")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
+
+    }
+    @Test
+    void deleteUser() {
+
+        given()
+                .log().uri()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+
+    }
+
+    @Test
+    void loginUnsuccessful() {
+
+        String data = "{ \"email\": \"sydney@fife\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+
+    }
+}
+
